8e7eca2c992320a60a4b71fe6a616a84e5e74efc
carlos.perez <carlos.perez@COLBOGITDEV-1>
2018-05-04 17:24:41 -0500

    avance

--- Pemarsa.Web/Program.cs
+++ Pemarsa.Web/Program.cs
@@ -14,12 +14,25 @@ namespace Pemarsa.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+                      .UseKestrel()
+                      .UseContentRoot(Directory.GetCurrentDirectory())
+                      .UseIISIntegration()
+                      .UseStartup<Startup>()
+                      .UseApplicationInsights()
+                      .Build();
+
+            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+        //}
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
     }
 }
--- Pemarsa.Web/Startup.cs
+++ Pemarsa.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -11,24 +12,53 @@ namespace Pemarsa.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //// This method gets called by the runtime. Use this method to add services to the container.
+        //// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //}
+
+        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+
+        //    app.Run(async (context) =>
+        //    {
+        //        await context.Response.WriteAsync("Hello World!");
+        //    });
+        //}
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
             });
+
+            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseStaticFiles();
+
         }
     }
 }
