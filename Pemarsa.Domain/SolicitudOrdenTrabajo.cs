f0c5f5bf1cf5c662befe3aedae41c8de7755ca63
cristian.cardona <cristian.cardona@COLBOGITDEV-2>
2018-07-09 16:14:34 -0500

    documento adjunto en remison
    Elementos de trabajo relacionados: #149

--- Pemarsa.Domain/SolicitudOrdenTrabajo.cs
+++ Pemarsa.Domain/SolicitudOrdenTrabajo.cs
@@ -7,15 +7,12 @@ using System.Text;
 namespace Pemarsa.Domain
 {
     public class SolicitudOrdenTrabajo : Entity
-    {    
+    {
         [Required]
         public int Cantidad { get; set; }
 
         [Required]
         public int CantidadInspeccionar { get; set; }
-
-        [ForeignKey("Cliente")]
-        public int ClienteId { get; set; }
 
         [Required]
         public string Contacto { get; set; }
@@ -24,35 +21,36 @@ namespace Pemarsa.Domain
 
         [Required]
         public string DetallesSolicitud { get; set; }
+        
+        [ForeignKey("Cliente")]
+        public int ClienteId { get; set; }
+        public virtual Cliente Cliente { get; set; }
 
         [ForeignKey("Estado")]
         public int EstadoId { get; set; }
+        public virtual Catalogo Estado { get; set; }
 
         [Required, ForeignKey("ClienteLinea")]
         public int LineaId { get; set; }
+        public virtual ClienteLinea ClienteLinea { get; set; }
 
         [Required, ForeignKey("OrigenSolicitud")]
         public int OrigenSolicitudId { get; set; }
-
+        public virtual Catalogo OrigenSolicitud { get; set; }
+        
         [Required, ForeignKey("Prioridad")]
         public int PrioridadId { get; set; }
-
+        public virtual Catalogo Prioridad { get; set; }
+        
         [ForeignKey("Responsable")]
         public int? ResponsableId { get; set; }
-
+        public virtual Catalogo Responsable { get; set; }
+        
+        [ForeignKey("Remision")]
+        public int? RemisionId { get; set; }
+        public virtual DocumentoAdjunto Remision { get; set; }
 
         public virtual ICollection<SolicitudOrdenTrabajoAnexos> Anexos { get; set; }
 
-        public virtual Cliente Cliente { get; set; }
-
-        public virtual Catalogo Estado { get; set; }
-
-        public virtual ClienteLinea ClienteLinea { get; set; }
-
-        public virtual Catalogo OrigenSolicitud { get; set; }
-
-        public virtual Catalogo Prioridad { get; set; }
-
-        public virtual Catalogo Responsable { get; set; }
     }
 }
